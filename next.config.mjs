9510eb01c3e8552c85f6e1e87a7eee31e39e9d38
Abdallah Elsobky <abdullah442286@fsc.bu.edu.eg>
2024-10-31 10:09:21 +0300

    use firebase hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 const nextConfig = {
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // Disables Next.js image optimization for static export compatibility
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp3|wav)$/i,
